802b05eb08966c643698d602c88a9c16b90986c9
StelaKhanamiryan <stellakhanamiryan01@gmail.com>
2024-05-11 22:33:20 +0400

    Done all exercises

---
+++ Homeworks/Class.cs
@@ -0,0 +1,58 @@
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
+
+//namespace Homeworks
+//{
+//    public class Class
+    //{
+        // Գրել ֆունկցիա որը արգումենտում ստանում է 2 զանգված և վերադարձնում դրանցից ստացված լիստ ՝ աճման կարգով
+
+        //public List<int> MergeAndSort(int[] arr1, int[] arr2)
+        //    {
+        //        List<int> mergedList = new List<int>(); 
+        //        for (int i = 0; i < arr1.Length; i++)
+        //        {
+        //            mergedList.Add(arr1[i]);
+        //        }
+        //        for (int i = 0; i < arr2.Length; i++ )
+        //        {
+        //            mergedList.Add(arr2[i]);
+        //        }
+
+        //        for (int i = 0; i < mergedList.Count; i++)
+        //        {
+        //            for (int j = 0; j < mergedList.Count; j++)
+        //            {
+        //                if (mergedList[i] < mergedList[j])
+        //                {
+        //                    int temp = mergedList[i];
+        //                    mergedList[i] = mergedList[j];
+        //                    mergedList[j] = temp;
+
+        //                }
+        //            }
+        //        }
+        //        return mergedList;
+        //    }
+
+
+        // --------------------------------------------------------------------------------------------------------------
+        //Օգտագործելով ստատիկ մեթոդ պարզել թե կլասի քանի օբյեկտ է ստեղծվել
+
+
+        //public static int count = 0;
+
+        //public Class()
+        //{
+        //    count++;
+        //}
+
+        //public static int objectCount()
+        //{
+        //    return count;
+        //}
+    //}
+//}
--- Homeworks/Program.cs
+++ Homeworks/Program.cs
@@ -1,2 +1,38 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Homeworks;
+
+// Գրել ֆունկցիա որը արգումենտում ստանում է 2 զանգված և վերադարձնում դրանցից ստացված լիստ ՝ աճման կարգով
+//Class class1 = new Class();
+
+//int[] arr1 = new int[3];
+//arr1[0] = 8;
+//arr1[1] = 12;
+//arr1[2] = 101;
+
+//int[] arr2 = new int[4];
+
+//arr2[0] = 19;
+//arr2[1] = 3;
+//arr2[2] = 45;
+//arr2[3] = 123;
+
+//List<int> list = class1.MergeAndSort(arr1, arr2);
+
+//foreach (int i in list)
+//{
+//    Console.WriteLine(i);
+//}
+
+
+// --------------------------------------------------------------------------------------------------------------
+//Օգտագործելով ստատիկ մեթոդ պարզել թե կլասի քանի օբյեկտ է ստեղծվել
+
+//Class ob1 = new Class();
+//Class ob2 = new Class();
+//Class ob3 = new Class();
+//Class ob4 = new Class();
+
+
+
+//// Get the count using the static method
+//int count = Class.objectCount();
+//Console.WriteLine($"Number of objects created: {count}");
