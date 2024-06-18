368a25abc26e40eea39cf3c5d90c460d93087c9f
StelaKhanamiryan <stellakhanamiryan01@gmail.com>
2024-06-18 17:39:08 +0400

    Homework is done

---
+++ Homeworks/Class.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homeworks
+{
+    public class Data
+    {
+       
+        public string path = @"C:\Users\Home\Desktop\homework\data.txt";
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public int Age { get; set; }
+        public Address Address { get; set; }
+
+        public void PrintData()
+        {
+            Address = new Address();
+            using (StreamReader sr = new StreamReader(path))
+            {
+                Name = sr.ReadLine();
+                Surname = sr.ReadLine();
+                Age = Convert.ToInt32(sr.ReadLine());
+                Address.Country = sr.ReadLine();
+                Address.City = sr.ReadLine();
+                Address.Street = sr.ReadLine();
+                Address.Apartment = sr.ReadLine();
+            }
+        }
+    }
+    public class Person
+    {
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public int Age { get; set; }
+        public Address MyAddress { get; set; }
+    }
+    public class Address
+    {
+        public string Country { get; set; }
+        public string City { get; set; }
+        public string Street { get; set; }
+        public string Apartment { get; set; }
+
+    }
+
+}
+
--- Homeworks/Program.cs
+++ Homeworks/Program.cs
@@ -1,2 +1,43 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Homeworks;
+
+Data data = new Data();
+List<Person> people = new List<Person>();
+
+using (StreamReader sr = new StreamReader(data.path))
+{
+    while (!sr.EndOfStream)
+    {
+        Person person = new Person
+        {
+            Name = sr.ReadLine(),
+            Surname = sr.ReadLine(),
+            Age = Convert.ToInt32(sr.ReadLine()),
+            MyAddress = new Address
+            {
+                Country = sr.ReadLine(),
+                City = sr.ReadLine(),
+                Street = sr.ReadLine(),
+                Apartment = sr.ReadLine()
+            }
+        };
+
+        people.Add(person);
+
+        if (!sr.EndOfStream)
+        {
+            sr.ReadLine();
+        }
+    }
+}
+foreach (var person in people)
+{
+    Console.WriteLine(person.Name);
+    Console.WriteLine(person.Surname);
+    Console.WriteLine(person.Age);
+    Console.WriteLine(person.MyAddress.Country);
+    Console.WriteLine(person.MyAddress.City);
+    Console.WriteLine(person.MyAddress.Street);
+    Console.WriteLine(person.MyAddress.Apartment);
+    Console.WriteLine();
+}
+
